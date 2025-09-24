f5f81ebe4faa0ccc9d0d92363fb2f5a891ceebb3
Adrien Denat <oi@adriendenat.com>
2025-09-24 02:07:37 +0100

    rollback to yarn since medusa doesn't support pnpm

--- medusa/medusa-config.ts
+++ medusa/medusa-config.ts
@@ -9,7 +9,7 @@ module.exports = defineConfig({
   projectConfig: {
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
+    // redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -19,26 +19,26 @@ module.exports = defineConfig({
     }
   },
   modules: [
-    {
-      resolve: "@medusajs/medusa/cache-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/event-bus-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/workflow-engine-redis",
-      options: {
-        redis: {
-          url: process.env.REDIS_URL,
-        },
-      },
-    },
+    // {
+    //   resolve: "@medusajs/medusa/cache-redis",
+    //   options: {
+    //     redisUrl: process.env.REDIS_URL,
+    //   },
+    // },
+    // {
+    //   resolve: "@medusajs/medusa/event-bus-redis",
+    //   options: {
+    //     redisUrl: process.env.REDIS_URL,
+    //   },
+    // },
+    // {
+    //   resolve: "@medusajs/medusa/workflow-engine-redis",
+    //   options: {
+    //     redis: {
+    //       url: process.env.REDIS_URL,
+    //     },
+    //   },
+    // },
     {
       resolve: "@medusajs/medusa/payment",
       options: {
