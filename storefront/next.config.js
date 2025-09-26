8f4d5c983861d5a13ca86ec729eb6b0fdcc05eff
Adrien Denat <oi@adriendenat.com>
2025-09-26 01:40:36 +0100

    created a bucket with a . :facepalm:

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "bailefun.fun.s3.eu-west-2.amazonaws.com",
+        hostname: "s3.eu-west-2.amazonaws.com/bailefun.fun",
       }
     ],
   },
