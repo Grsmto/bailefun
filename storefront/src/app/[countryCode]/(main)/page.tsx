6bab6fba5d07ea938468724f103bf1c9550ca2c4
Adrien Denat <oi@adriendenat.com>
2026-02-19 00:15:15 +0000

    fix title and favicon

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,9 @@ import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "BAILEFUN",
   description:
-    "A performant frontend ecommerce starter template with Next.js 15 and Medusa.",
+    "T-shirts designed in London for brasileiros",
 }
 
 export default async function Home(props: {
