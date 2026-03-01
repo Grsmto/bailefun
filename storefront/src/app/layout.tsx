b76f1d84d62095ecd88c88b33bd1a0cb68cfd3dc
Adrien Denat <oi@adriendenat.com>
2026-03-01 11:37:58 +0000

    fix observerKit init 2x

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,15 +1,11 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
-import * as ObserverKit from "@maximebonhomme/observerkit-js"
+
 import "styles/globals.css"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
-
-ObserverKit.init({
-  apiKey: "ok_proj_b712058121ff46e2d36ca6b5d44aa130",
-})
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
