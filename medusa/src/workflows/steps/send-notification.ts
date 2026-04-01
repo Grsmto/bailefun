fac7cd836dd49f74d4bc4b676ec4c41bd6b50c11
Adrien Denat <oi@adriendenat.com>
2026-04-02 00:09:29 +0100

    fix ts error

--- medusa/src/workflows/steps/send-notification.ts
+++ medusa/src/workflows/steps/send-notification.ts
@@ -1,13 +1,13 @@
 import { Modules } from "@medusajs/framework/utils"
 import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk"
-import { CreateNotificationDTO } from "@medusajs/framework/types"
+import { CreateNotificationDTO, INotificationModuleService } from "@medusajs/framework/types"
 
 export const sendNotificationStep = createStep(
     "send-notification",
     async (data: CreateNotificationDTO[], { container }) => {
         const notificationModuleService = container.resolve(
             Modules.NOTIFICATION
-        )
+        ) as INotificationModuleService
         const notification = await notificationModuleService.createNotifications(data)
         return new StepResponse(notification)
     }
