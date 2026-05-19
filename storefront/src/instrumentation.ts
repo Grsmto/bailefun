f73a35ac55b0269a6f64ae7fff651a079d7b53e6
Adrien Denat <oi@adriendenat.com>
2026-05-19 11:08:01 +0100

    update observerkit

--- storefront/src/instrumentation.ts
+++ storefront/src/instrumentation.ts
@@ -1,3 +1,7 @@
 import * as ObserverKit from "@maximebonhomme/observerkit-js"
 
+ObserverKit.init({
+  apiKey: "ok_proj_b712058121ff46e2d36ca6b5d44aa130",
+})
+
 export const onRequestError = ObserverKit.captureException;
